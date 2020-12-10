b31f24dd242119f7ea17bbb0aa3f4ff6c8d55436
Lillian Paris <lillian.paris529@gmail.com>
2020-12-09 19:11:49 -0700

    Added to api.js

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,20 @@ const router = require("express").Router();
 
 const Workout = require("../models/workouts.js");
 
+// Grab workouts
+
+router.get("/api/workouts", (req, res) => {
+    Workout.find({})
+        .then((workoutResponse) => {
+            res.json(workoutResponse);
+        })
+        .catch((err) => {
+            res.status(400).json(err);
+        });
+});
+
+// create a new workout 
+
 router.post("/api/workouts", ({ body }, res) => {
     Workout.create(body)
         .then(dbWorkout => {
@@ -12,11 +26,12 @@ router.post("/api/workouts", ({ body }, res) => {
         });
 });
 // Backend uses : and variable, front end uses + and variable
+// update one workout
 router.put("/api/workouts/:id", ({ body }, res) => {
     Workout.updateOne({ _id: req.params.id }, { exercises: body })
-    .then(function (dbImage) {
-        res.json(dbImage);
-    });
+        .then(function (dbImage) {
+            res.json(dbImage);
+        });
 });
 
 router.get("/api/workouts/range", (req, res) => {
